8c73f4052b45a4b4e21efae149af2350147bfb94
John Brooks <john.brooks@dereferenced.net>
2016-11-15 14:12:47 -0700

    Add correctness and benchmark tests, and test data
    The test verifies that calculated hashes match other implementations. A
    benchmark test is also included.
    
    testdata is identical to the C implementation from:
    
    https://github.com/jonasob/blockhash/

--- blockhash_test.go
+++ blockhash_test.go
@@ -1,16 +1,75 @@
 package main
 
 import (
+	"bytes"
+	"fmt"
 	"image"
 	_ "image/jpeg"
+	"io/ioutil"
 	"os"
 	"testing"
 )
 
-func BenchmarkBlockHash(t *testing.B) {
-	f, _ := os.Open("test_data/1.jpg")
-	defer f.Close()
+func readTestFileMap(path string) (testFiles map[string]string, err error) {
+	data, err := ioutil.ReadFile(path)
+	if err != nil {
+		err = err
+		return
+	}
 
-	i, _, _ := image.Decode(f)
-	_ = Blockhash(i, 16)
+	testFiles = make(map[string]string)
+	for _, line := range bytes.Split(data, []byte{'\n'}) {
+		if len(line) == 0 {
+			continue
+		}
+		v := bytes.Split(line, []byte{' '})
+		if len(v) != 3 {
+			err = fmt.Errorf("Unexpected format in test file line: '%s'", line)
+			return
+		}
+		testFiles[string(v[2])] = string(v[0])
+	}
+	return
+}
+
+func TestBlockHash(t *testing.T) {
+	testFiles, err := readTestFileMap("testdata/images/exact-hashes.txt")
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	for file, expectedHash := range testFiles {
+		t.Run(file, func(t *testing.T) {
+			f, _ := os.Open("testdata/images/" + file)
+			defer f.Close()
+
+			i, _, _ := image.Decode(f)
+			result := Blockhash(i, 16)
+
+			if result != Hash(expectedHash) {
+				distance := result.Distance(Hash(expectedHash))
+				t.Errorf("Hash did not match (distance=%d)\nExpected: %s\nResult:   %s\n", distance, expectedHash, result)
+			}
+		})
+	}
+}
+
+func BenchmarkBlockHash(b *testing.B) {
+	testFiles, err := readTestFileMap("testdata/images/exact-hashes.txt")
+	if err != nil {
+		b.Fatal(err)
+	}
+
+	for file, _ := range testFiles {
+		b.Run(file, func(b *testing.B) {
+			f, _ := os.Open("testdata/images/" + file)
+			defer f.Close()
+			image, _, _ := image.Decode(f)
+
+			b.ResetTimer()
+			for i := 0; i < b.N; i++ {
+				_ = Blockhash(image, 16)
+			}
+		})
+	}
 }
